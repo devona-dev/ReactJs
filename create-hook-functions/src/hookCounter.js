8c1874ee953789ffd9fe9dfecadbd13e0e6b405b
devona-dev <mamka.islomv@gmail.com>
2022-09-03 09:21:49 +0500

    create hook functions

---
+++ create-hook-functions/src/hookCounter.js
@@ -0,0 +1,17 @@
+// import React, { useState } from "react";
+
+// function HookCounter() {
+//     const [count, setCount] = useState(7); // 2 ta elementdan iborat qator qaytaradi. 1-ozgaruvchan, 2-function
+
+//     /*
+//      const count = countState[o]; // 1 elementda ozgaruvchining ozi saqlanadi
+//      const setCount = countState[1]; // 2 elementda funktsiya saqlanadi
+//     */
+//     return(
+//         <div>
+//             <button onClick={() => setCount(count + 1)}>Count {count}</button>
+//         </div>
+//     )
+// }
+
+// export default HookCounter;
---
+++ create-hook-functions/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import ClassCounter from './classCounter';
+// import HookCounter from './hookCounter';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
